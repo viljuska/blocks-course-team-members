4f45074f1a648a13b1eb1174b317ffb66b0a0300
stefanjocic <stefan.jocic@maksimer.com>
2022-09-20 14:21:32 +0200

    lesson 46

--- src/edit.js
+++ src/edit.js
@@ -3,8 +3,19 @@ import './editor.scss';
 
 export default function Edit() {
 	return (
-		<div {...useBlockProps()}>
-			<InnerBlocks allowedBlocks={['blocks-course/team-member']} />
+		<div { ...useBlockProps() }>
+			<InnerBlocks
+				allowedBlocks={ [ 'blocks-course/team-member' ] }
+				template={ [
+					// With prefilled
+					// [ 'blocks-course/team-member', {
+					// 	name: 'Member 1', bio: 'Bio 1'
+					// } ],
+					// Empty
+					[ 'blocks-course/team-member' ],
+				] }
+				// templateLock="insert"
+			/>
 		</div>
 	);
 }

e17a29decde6b8e0947653f58be4068bb8415ee7
stefanjocic <stefan.jocic@maksimer.com>
2022-09-20 13:47:15 +0200

    lesson 44

---
+++ src/team-member/index.js
@@ -0,0 +1,15 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { __ } from '@wordpress/i18n';
+
+registerBlockType('blocks-course/team-member', {
+	title: __('Team Member', 'team-members'),
+	description: __('Team Member desc', 'team-members'),
+	icon: 'admin-users',
+	parent: ['blocks-course/team-members'],
+	edit: () => {
+		return <p>edit</p>;
+	},
+	save: () => {
+		return <p>save</p>;
+	},
+});

b496d703746b861e389f9b8f08aa13db01fae626
stefanjocic <stefan.jocic@maksimer.com>
2022-09-20 15:29:16 +0200

    lesson 51

--- src/team-member/edit.js
+++ src/team-member/edit.js
@@ -1,10 +1,29 @@
 import { useBlockProps, RichText, MediaPlaceholder } from '@wordpress/block-editor';
+import { isBlobURL }                                 from '@wordpress/blob';
+import { Spinner }                                   from '@wordpress/components';
 import { __ }                                        from '@wordpress/i18n';
 
 export default function edit( { attributes, setAttributes } ) {
-	const { name, bio } = attributes,
+	const { name, bio, url, alt } = attributes,
 		onChangeName = newName => setAttributes( { name: newName } ),
-		onChangeBio = newBio => setAttributes( { bio: newBio } );
+		onChangeBio = newBio => setAttributes( { bio: newBio } ),
+		onSelectImage = image => {
+			if ( !image || !image.url ) {
+				setAttributes( {
+					url: undefined,
+					id: undefined,
+					alt: ''
+				} );
+
+				return;
+			}
+
+			setAttributes( {
+				url: image.url,
+				id: image.id,
+				alt: image.alt
+			} );
+		};
 
 	return <div { ...useBlockProps() }>
 		<RichText
@@ -21,13 +40,21 @@ export default function edit( { attributes, setAttributes } ) {
 			value={ bio }
 			allowedFormats={ [] }
 		/>
+		{ url &&
+			<div className={ `wp-block-blocks-course-team-member-img ${ isBlobURL( url ) ? 'is-loading' : '' }` }>
+				<img src={ url } alt={ alt } />
+				{ isBlobURL( url ) && <Spinner /> }
+				<Spinner />
+			</div>
+		}
 		<MediaPlaceholder
 			icon="admin-users"
-			onSelect={ val => console.log( val ) }
+			onSelect={ onSelectImage }
 			onSelectUrl={ val => console.log( val ) }
 			onError={ err => console.error( err ) }
 			accept="image/*"
 			allowdTypes={ [ 'image' ] }
+			disableMediaButtons={ url }
 		/>
 	</div>;
 }
